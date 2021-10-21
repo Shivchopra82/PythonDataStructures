f89c98dc5617085a133756b6e628412fa2a80208
Shiv Chopra <shivchopra82@gmail.com>
2021-10-21 12:52:09 +0530

    bubble sort completed

--- bubblesort.py
+++ bubblesort.py
@@ -1,13 +1,21 @@
-# Sorting is the method for arranging the list of numbers in desired order.
+#1.Sorting is the method for arranging the list of numbers in desired order.
 #2.Time complexity is order of n^2 
+
+
 def bub_sort(num_list):
     size = len(num_list)
     for i in range(size-1):
+        sort = False
         for j in range(size-1-i):
             if num_list[j] > num_list[j+1]:
                 temp = num_list[j]
                 num_list[j] = num_list[j+1]
                 num_list[j+1] = temp
+        if not sort:
+            break
+
+# This bub_sort function sorts the list in its place. We have used a bool variable to test if the list is already sorted or not.
+# if we found it already sorted then we dont need to carryon the whole the whole algorythm . 
 
 
 
