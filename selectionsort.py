7492a26e0abe8b23e31f3c3c791f51677d7efa48
Shiv Chopra <shivchopra82@gmail.com>
2021-10-24 11:08:32 +0530

    selection sort completed

--- selectionsort.py
+++ selectionsort.py
@@ -3,11 +3,15 @@
 
 def select_sort(lst):
     size = len(lst)
-    for i in range(size):
+    for i in range(size-1):
         min_index = i
-        for j in range(min)
+        for j in range(min_index+1,size):
+            if lst[j] < lst[min_index]:
+                min_index = j
+        if i != min_index:
+            lst[i], lst[min_index] = lst[min_index], lst[i]
 
 
 elements = [1,2,5,6,4,9,22,11,25,36,24,25,29,28,27,30,36,15,0,25,64,95,15,46,25,35,369,54,695,262,364,621,000,264,6775,41]
-min_ele(elements)
+select_sort(elements)
 print(elements)
