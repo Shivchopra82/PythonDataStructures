b5883ae337c8d96565170a33b765f2b7ff3ce43e
Shiv Chopra <shivchopra82@gmail.com>
2021-10-24 11:03:15 +0530

    selection sort started

---
+++ selectionsort.py
@@ -0,0 +1,13 @@
+# 1. In each iteration we compare the element with starting element and change the position accordingly. 
+# 2. Thus in each iteration we have a sorted array on left handside and unsorted array on right hand side. 
+
+def select_sort(lst):
+    size = len(lst)
+    for i in range(size):
+        min_index = i
+        for j in range(min)
+
+
+elements = [1,2,5,6,4,9,22,11,25,36,24,25,29,28,27,30,36,15,0,25,64,95,15,46,25,35,369,54,695,262,364,621,000,264,6775,41]
+min_ele(elements)
+print(elements)
